e02d54c984e441276a45a2d727b09abff7637a19
Philipp Kreuzbichler <kreuzbichler.ph@gmail.com>
2026-06-04 11:50:32 +0200

    other: use flutters built-in kotlin support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -13,10 +11,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
